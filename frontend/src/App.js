c2d914d00ab8b84a7ed8c9c64057476e88710802
AndresPradaMurillo <89157620+AndresPradaMurillo@users.noreply.github.com>
2023-12-01 17:21:00 -0600

    Version primitiva de local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,24 @@ function App() {
   
   const [googleId, setGoogleId] = useState(null);
 
+  let letras = [{
+    "nombre" : "Cancion 1", 
+    "Letra" : "Baby, we're the new romantics"
+  },
+  {
+    "nombre" : "Cancion 2", 
+    "Letra" : [
+      "We need love",
+      "But all we want is danger",
+      "We team up",
+      "Then switch sides like a record changer",
+      "The rumors are terrible and cruel",
+      "But honey, most of them are true"
+    ]
+  }];
+
+  //localStorage.setItem('111045705164865416538', JSON.stringify(letras));
+
   useEffect(() => {
     function start() {
       gapi.client.init({
@@ -35,6 +53,18 @@ function App() {
         <div>
           <h1>Identificador del usuario:</h1>
           <h2>{googleId}</h2>
+          <h3>Canciones guardadas: {localStorage.getItem(googleId)}</h3>
+
+
+
+
+
+
+
+
+
+
+
         </div>
       ) : (
         <div className='login-card'>
