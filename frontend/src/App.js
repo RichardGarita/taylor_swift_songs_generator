74aa47f1594f6f0984a8e7ccb351e668640d70a9
AndresPradaMurillo <89157620+AndresPradaMurillo@users.noreply.github.com>
2023-12-01 21:28:38 -0600

    Se agrega un sidebar que lista todas las canciones almacenadas en el local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,34 +8,8 @@ const clientId = "759143959718-add95sbrvk9elamjjn4331vnk15hrf30.apps.googleuserc
 
 function App() {
 
-  const onSuccess = (res) => {
-    console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
-    setGoogleId(res.profileObj.googleId);
-  }
-
-  const onFailure = (res) => {
-    console.log("LOGIN FAILED! res: ", res);
-  }
-  
   const [googleId, setGoogleId] = useState(null);
-
-  let letras = [{
-    "nombre" : "Cancion 1", 
-    "Letra" : "Baby, we're the new romantics"
-  },
-  {
-    "nombre" : "Cancion 2", 
-    "Letra" : [
-      "We need love",
-      "But all we want is danger",
-      "We team up",
-      "Then switch sides like a record changer",
-      "The rumors are terrible and cruel",
-      "But honey, most of them are true"
-    ]
-  }];
-
-  //localStorage.setItem('111045705164865416538', JSON.stringify(letras));
+  const [canciones, setCanciones] = useState([]);
 
   useEffect(() => {
     function start() {
@@ -47,24 +21,32 @@ function App() {
     gapi.load('client:auth2', start);
   });
 
+  const onSuccess = (res) => {
+    console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
+    setGoogleId(res.profileObj.googleId);
+    setCanciones(JSON.parse(localStorage.getItem(res.profileObj.googleId)));
+  }
+
+  const onFailure = (res) => {
+    console.log("LOGIN FAILED! res: ", res);
+  }
+
+  const cancionesSideBar = canciones.map((cancion) =>
+    <a href="#">{cancion.nombre}</a>
+  );
+  
   return (
     <div>
       {googleId ? (
-        <div>
-          <h1>Identificador del usuario:</h1>
-          <h2>{googleId}</h2>
-          <h3>Canciones guardadas: {localStorage.getItem(googleId)}</h3>
-
-
-
-
-
-
-
-
-
-
-
+        <div>      
+          <div class="sidebar">
+            {cancionesSideBar}
+          </div>
+          <div class="content">
+            <h1>Identificador del usuario:</h1>
+            <h2>{googleId}</h2>
+            <h3>Canciones guardadas: {localStorage.getItem(googleId)}</h3>
+          </div>
         </div>
       ) : (
         <div className='login-card'>
